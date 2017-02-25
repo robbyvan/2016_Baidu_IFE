f7c3019a2c2a725d689863e1fc48c562a35083ea
robbyvan <fzpinus@gmail.com>
2017-02-25 10:55:29 -0800

    tree widget

--- stage2/task13/revise/app2.js
+++ stage2/task13/revise/app2.js
@@ -1,22 +1,51 @@
-$ = function(el){
+/*
+* Shorthand for querySelctor
+*/
+const $ = function(el){
   return document.querySelector(el);
 }
 
+/*
+* Browser Compatibility
+*/
+const addEvent = function(element, type, handler){
+  if (element.addEventListener){
+    element.addEventListener(type, handler, false);
+  }else if (element.attachEvent){
+    element.attachEvent('on' + type, handler);
+  }else{
+    element['on' + type] = handler;
+  }
+}
+
+/*
+*   =========================== TreeNode Start  ===========================
+*   Create a Tree Widget to the output field
+
+*   @param {Object} nodeConfig
+*   @param {String} outputId
+
+*   Example:
+    const root = new TreeNode({nodeContent: 'Countries'}, 'tree-panel');
+*
+*/
 
 function TreeNode(nodeConfig, outputId){
   if (outputId !== undefined){
     this.output = $('#' + outputId);
   }
   this.children = nodeConfig.children || [];
-  this.parent = nodeConfig.parent;
+  this.parent = nodeConfig.parent || null;
   this.nodeContent = nodeConfig.nodeContent || "";
   this.domElement = nodeConfig.domElement || null;
+
   if (this.domElement){
     this.domElement.TreeNode = this;  
   }
 }
 
 TreeNode.prototype = {
+  constructor: TreeNode,
 
   init: function(rootName){
     let self = this;
@@ -26,7 +55,7 @@ TreeNode.prototype = {
 
   createSearchField: function(){
     let searchField = document.createElement('div');
-      searchField.className = 'tree-search';
+      searchField.className = 'tree-search-field';
 
       let input = document.createElement('input');
       input.className = 'tree-search-input';
@@ -39,6 +68,32 @@ TreeNode.prototype = {
       searchField.appendChild(input);
       searchField.appendChild(button);
 
+      /*
+      *  Seach Field Events
+      *  Bind click event to button
+      */
+      addEvent(button, 'click', (e) => {
+        e.stopPropagation();
+
+        //clear styles of last search
+        for (let i = 0; i < root.matches.length; ++i){
+          root.matches[i].domElement.children[0].getElementsByClassName('node-content')[0].classList.remove('found');
+        }
+
+        let userInput = $('.tree-search-input').value;
+        $('.tree-search-input').value = "";
+        if (userInput === ""){
+          return;
+        }
+
+        root.matches = root.dfsSearch(userInput, []);
+
+        //render
+        for (let i = 0; i < root.matches.length; ++i){
+          root.matches[i].domElement.children[0].getElementsByClassName('node-content')[0].className += ' found';
+        }
+      });
+
       this.output.appendChild(searchField);
   },
 
@@ -49,14 +104,14 @@ TreeNode.prototype = {
     let label = document.createElement('label');
 
     let expandIcon = document.createElement('label');
-    expandIcon.className = 'expand-icon valid-click';
+    expandIcon.className = 'fa fa-angle-down expand-icon valid-click';
 
     let nodeContent = document.createElement('span');
     nodeContent.className = 'node-content valid-click';
     nodeContent.textContent = rootContent;
 
     let addIcon = document.createElement('i');
-    addIcon.className = 'add-icon valid-click';
+    addIcon.className = 'fa fa-plus add-icon valid-click';
 
     label.appendChild(expandIcon);
     label.appendChild(nodeContent);
@@ -64,8 +119,43 @@ TreeNode.prototype = {
     root.appendChild(label);
 
     this.output.appendChild(root);
-    this.domElement =  root;
+    this.domElement = root;
     this.domElement.TreeNode = this;
+    this.matches = [];
+
+    /*  
+    * Tree Field Events
+    * Bind all click Events to root
+    */
+    addEvent(root, 'click', (e) => {
+      e.stopPropagation();
+      let clickedElem = e.target || e.srcElement;
+
+      if (clickedElem.className.indexOf('valid-click') === -1){
+        console.log('invalid click');
+        return;
+      }
+
+      //check what is clicked
+      console.log('valid-click');
+      // console.log(clickedElem);
+      if (clickedElem.className.indexOf('add-icon') !== -1){
+        //Add
+        let text = prompt('Add a node.');
+        console.log(text);
+        if (text){
+          clickedElem.parentNode.parentNode.TreeNode.addTreeNode(text);  
+        }
+      }else if (clickedElem.className.indexOf('del-icon') !== -1){
+        //Delete
+        clickedElem.parentNode.parentNode.TreeNode.delTreeNode();
+      }else{
+        //Toggle
+        clickedElem.parentNode.parentNode.TreeNode.toggleNode();
+      }
+    });
+
+    
     return this;
   },
 
@@ -86,17 +176,17 @@ TreeNode.prototype = {
     let label = document.createElement('label');
 
     let expandIcon = document.createElement('i');
-    expandIcon.className = 'expand-icon valid-click';
+    expandIcon.className = 'fa fa-angle-down expand-icon valid-click';
 
     let nodeContent = document.createElement('span');
     nodeContent.className = 'node-content valid-click';
     nodeContent.textContent = text;
 
     let addIcon = document.createElement('i');
-    addIcon.className = 'add-icon valid-click';
+    addIcon.className = 'fa fa-plus add-icon valid-click';
 
     let delIcon = document.createElement('i');
-    addIcon.className = 'del-icon valid-click';
+    delIcon.className = 'fa fa-minus del-icon valid-click';
 
     label.appendChild(expandIcon);
     label.appendChild(nodeContent);
@@ -129,20 +219,49 @@ TreeNode.prototype = {
   },
 
   toggleNode: function(){
-    if (this.domElement.className.indexOf('expand') !== -1){
-      this.domElement.classList.remove('expand');
+    if (this.domElement.getElementsByClassName('expand-icon')[0].className.indexOf('down') !== -1){
+      for (let i = 0; i < this.children.length; ++i){
+        this.children[i].domElement.className += ' hide';
+      }
+      this.domElement.getElementsByClassName('expand-icon')[0].className = this.domElement.getElementsByClassName('expand-icon')[0].className.replace(/down/, 'right');
     }else{
-      this.domElement.className += ' expand';
+      for (let i = 0; i < this.children.length; ++i){
+        this.children[i].domElement.classList.remove('hide');
+      }
+      this.domElement.getElementsByClassName('expand-icon')[0].className = this.domElement.getElementsByClassName('expand-icon')[0].className.replace(/right/, 'down');
     }
     return this;
-  }
+  },
+
+  dfsSearch: function(target, found){
+    let self = this;
+    if (!self){
+      return;
+    }
+
+    if (self.nodeContent.trim().toLowerCase() === target.trim().toLowerCase()){
+      found.push(self);
+    }
+
+    for (let i = 0; i < self.children.length; ++i){
+      self.children[i].dfsSearch(target, found);
+    }
+    return found;
+  },
 
 };
 
-var root = new TreeNode({parent: null, nodeContent: 'Programmers'}, 'tree-panel');
+/*
+*   =========================== TreeNode End  ===========================
+*/
+
+
+/* Instantiate root */
+const root = new TreeNode({nodeContent: 'Programmers'}, 'tree-panel');
 root.init(root.nodeContent);
+
+//Add Initial nodes
 root.addTreeNode('Web Developer');
 root.addTreeNode('Software Engineer');
 root.children[0].addTreeNode('Front-End Developer');
-// root.children[1].delTreeNode();
-root.toggleNode();
+
